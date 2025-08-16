66ade5d9c475329e8da13670479b191999cb03e4
Your Namecd <shaharulsiyam0273@gmail.com>
2025-08-16 12:57:02 +0600

    s

--- src/middleware/prouct_image_upload.ts
+++ src/middleware/prouct_image_upload.ts
@@ -90,7 +90,7 @@ const upload_product_image = () => {
     }
   };
 
-  const maxVideoLength = 20;
+  const maxVideoLength = 5000;
 
   const upload = multer({
     limits: { fileSize: 5000 * 1024 * 1024 },
