dd63ee60b58f4734c64792695ba5b98a9260dc0c
shaharulsiyam <shaharulsiyam0273@gmail.com>
2025-09-15 09:17:20 +0600

    seedadmin

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 import config from "../DefaultConfig/config";
+import { seedAdmin } from '../utils/seedAdmin';
 
 export const connectToDB = async () => {
   try {
     await mongoose.connect(config?.DATABASE_URL || "", {
-      dbName: config?.DB_NAME,
+      dbName: "testdb" //config?.DB_NAME,
     });
     console.log("connected to database");
+    await seedAdmin();
   } catch (error) {
     console.log(error);
   }
