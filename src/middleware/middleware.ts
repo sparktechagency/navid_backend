83c0cedbe1e14b7d3273d039ea3fa21be8e00195
mdshaharulsiyam <shaharulsiyam56@gmail.com>
2025-11-05 10:25:23 +0600

    cors

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -39,6 +39,7 @@ const middleware = (app: Express) => {
         "http://10.10.20.54:3000",
         "http://10.10.20.54:3001",
         "https://divandione.com",
+        "https://www.divandione.com"
       ],
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
       optionsSuccessStatus: 200,
