756e9d3bb0d51e95b15558136a42086c4ab2c408
manik <maniksarker652@gmail.com>
2026-02-07 16:35:55 +0600

    change file uploader

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 import config from "../DefaultConfig/config";
-import { seedAdmin } from '../utils/seedAdmin';
+import { seedAdmin } from "../utils/seedAdmin";
 
 export const connectToDB = async () => {
   try {
     await mongoose.connect(config?.DATABASE_URL || "", {
       // dbName: config?.DB_NAME, // testdatabase_02 change from hosain
-      dbName: 'testdatabase_03', // testdatabase_02 change from hosain
+      dbName: config?.DB_NAME, // testdatabase_02 change from hosain
     });
     console.log("connected to database");
     await seedAdmin();
